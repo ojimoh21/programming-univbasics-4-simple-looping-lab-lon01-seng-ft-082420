8d1618b43bd8a2cd7773aab7d059eebbdbc0463c
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-01 20:25:08 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,37 @@
-# Write your methods here
+
+def loop_message_five_times(string)
+  counter = 0 
+  while counter < 5
+  puts string
+  counter += 1
+  end 
+end 
+
+def loop_message_n_times(message, n)
+  counter = 0 
+  while counter < n 
+  puts message 
+  counter += 1 
+  end 
+end 
+  
+def output_array(array)
+  index = 0 
+  while array[index] do 
+    puts array[index]
+    index += 1 
+  end 
+end 
+
+
+def return_string_array(array)
+  index = 0 
+  new_array = []
+while array[index] do
+  element = array[index]
+  element = element.to_s
+  new_array.push(element)
+  index += 1
+end 
+new_array
+end 
